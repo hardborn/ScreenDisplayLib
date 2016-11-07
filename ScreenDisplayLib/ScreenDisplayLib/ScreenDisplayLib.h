30fdd0f05dd60e92199ea8200109949d56a94b50
hardborn <hardborn@live.cn>
2016-11-07 10:03:00 +0800

    添加工程项目

---
+++ ScreenDisplayLib/ScreenDisplayLib/ScreenDisplayLib.h
@@ -0,0 +1,13 @@
+// ScreenDisplayLib.h
+
+#pragma once
+
+using namespace System;
+
+namespace ScreenDisplayLib {
+
+	public ref class ScreenDisplay
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
